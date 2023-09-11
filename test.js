a3d3f6a6a4b528ad9780eaf4556c8f893ae64d05
mrozio13pl <41209892+mrozio13pl@users.noreply.github.com>
2023-09-12 00:09:28 +0200

    switched from `ava` to `uvu`

--- test.js
+++ test.js
@@ -1,23 +1,27 @@
+const { test } = require('uvu');
+const { is, throws, not } = require('uvu/assert');
+
 const repeat = require('.');
-const { default: test } = require('ava');
 
-test('simple tests for a repeated strings', (t) => {
-    t.deepEqual(repeat('a', 5), 'aaaaa');
-    t.deepEqual(repeat('cool', 10), 'cool'.repeat(10));
+test('simple tests for a repeated strings', () => {
+    is(repeat('a', 5), 'aaaaa');
+    is(repeat('cool', 10), 'cool'.repeat(10));
 });
 
-test('cache implementation', (t) => {
-    t.deepEqual(repeat('test', 7), 'test'.repeat(7));
-    t.deepEqual(repeat('test', 7), 'test'.repeat(7));
-    t.deepEqual(repeat('test', 6), 'test'.repeat(6));
+test('cache implementation', () => {
+    is(repeat('test', 7), 'test'.repeat(7));
+    is(repeat('test', 7), 'test'.repeat(7));
+    is(repeat('test', 6), 'test'.repeat(6));
 });
 
-test('test errors', (t) => {
-    t.throws(() => repeat(undefined, 10), {
+test('test errors', () => {
+    throws(() => repeat(undefined, 10), {
         message: 'Expected `input` to be a `string`, received undefined'
     });
-    t.throws(() => repeat(123, 10), {
+    throws(() => repeat(123, 10), {
         message: 'Expected `input` to be a `string`, received number'
     });
-    t.notThrows(() => repeat('str', 3));
+    not.throws(() => repeat('str', 3));
 });
+
+test.run();
